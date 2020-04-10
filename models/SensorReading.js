ed6b2702243ec2e7e5c45ceadf85f9ba5d93fde6
Thamal Dilanka <thamaldilanke@gmail.com>
2020-04-11 05:20:36 +0530

    Sensor reading schema modified

--- models/SensorReading.js
+++ models/SensorReading.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const sensorReadingSchema = new mongoose.Schema({
 	sensor: {
-		type: mongoose.Schema.Types.ObjectId,
+		type: String,
 		ref: 'Sensor',
 	},
 	reading: {
